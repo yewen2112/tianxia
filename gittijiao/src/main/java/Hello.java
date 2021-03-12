130d64e1590c919b1afd0d3681e2013f6a6a950b
yewen <2112682417@qq.com>
2021-03-12 19:25:30 +0800

    一个新创建的hello类

---
+++ gittijiao/src/main/java/Hello.java
@@ -0,0 +1,13 @@
+/**
+ * ClassName:Hello
+ * Package:PACKAGE_NAME
+ * Description: 描述信息
+ *
+ * @date:2021/3/12 19:22
+ * @author:王尚昆
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("你好啊，希望以后的以后能过的越来越好");
+    }
+}
